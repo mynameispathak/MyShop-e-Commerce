2429c0df1674a9ba495f42f515eeaba6b899b1c7
Aniket Pathak <171230012@nitdelhi.ac.in>
2021-02-09 05:07:12 +0530

    React App and React Bootstrap setup

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">
+                        Copyright &copy; 2021 MyShop
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default footer
