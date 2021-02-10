60d21bbfbcf0619a61b775dabb2cd03a0f4be1fd
Aniket Pathak <171230012@nitdelhi.ac.in>
2021-02-10 07:47:11 +0530

    Set up Product Detail Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
     <div className="App">
-        <Header/>
+      <Router>
+        <Header />
         <main className="py-3">
           <Container>
-            <h1>Latest Products</h1>
-            <HomeScreen/>
+            <Route path='/' component={HomeScreen} exact />
+            <Route path='/product/:id' component={ProductScreen} />
           </Container>
         </main>
-        <Footer/>
+        <Footer />
+      </Router>
     </div>
   );
 }
